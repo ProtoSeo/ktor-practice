ae53b9cac0d2912911eda71b5ece9dd8f5d08ed7
SeoSeunghun <proto_seo@naver.com>
2022-05-11 00:04:14 +0900

    [feat] application.kt 수정

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,6 +1,7 @@
 package com.example
 
-import com.example.config.setupConfig
+import com.example.config.configureDatabase
+import com.example.config.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,10 +9,6 @@ import io.ktor.server.routing.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-    setupConfig()
-    routing {
-        get("/") {
-            call.respondText("Hello, world!")
-        }
-    }
+    configureDatabase()
+    configureRouting()
 }
