f7c5214f64a98f9c5f63dbc23b514ac96b67b882
SeoSeunghun <proto_seo@naver.com>
2022-05-10 19:49:10 +0900

    [feat] item entity, ItemRepository 생성

---
+++ src/main/kotlin/com/example/item/domain/ItemRepository.kt
@@ -0,0 +1,52 @@
+package com.example.item.domain
+
+import com.example.item.dto.ItemRequestDto
+import com.example.item.dto.ItemResponseDto
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class ItemRepository {
+    init {
+        transaction {
+            SchemaUtils.create(Items)
+        }
+    }
+
+    fun findById(id: Long): ItemResponseDto {
+        return transaction {
+            Items.select { Items.id eq id }.map { row ->
+                ItemResponseDto(
+                    row[Items.id].value,
+                    row[Items.name],
+                    row[Items.price],
+                    row[Items.stockQuantity]
+                )
+            }.first()
+        }
+    }
+
+    fun create(itemDto: ItemRequestDto): Long {
+        return transaction {
+            Items.insertAndGetId { row ->
+                row[name] = itemDto.name
+                row[price] = itemDto.price
+                row[stockQuantity] = itemDto.stockQuantity
+            }.value
+        }
+    }
+
+    fun update(id: Long, itemDto: ItemRequestDto): ItemResponseDto {
+        transaction {
+            Items.update({ Items.id eq id }) { row ->
+                row[name] = itemDto.name
+                row[price] = itemDto.price
+                row[stockQuantity] = itemDto.stockQuantity
+            }
+        }
+        return findById(id)
+    }
+
+    fun clear() = Items.deleteAll()
+
+}
