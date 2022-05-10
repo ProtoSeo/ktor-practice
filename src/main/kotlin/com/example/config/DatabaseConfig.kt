19e9e8cccf53ab92aeb1ad98bf14418989946126
SeoSeunghun <proto_seo@naver.com>
2022-05-10 20:08:16 +0900

    [feat] database Config 생성

---
+++ src/main/kotlin/com/example/config/DatabaseConfig.kt
@@ -0,0 +1,13 @@
+package com.example.config
+
+import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
+
+fun Application.setupConfig() {
+    val databaseObject = environment.config.config("ktor.database")
+    val driver = databaseObject.property("driver").getString()
+    val url = databaseObject.property("url").getString()
+    val user = databaseObject.property("user").getString()
+    val password = databaseObject.property("password").getString()
+    Database.connect(url, driver, user, password)
+}
