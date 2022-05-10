84908cf1f1ba26b9f13d919d5fa52687d629e8b6
SeoSeunghun <proto_seo@naver.com>
2022-05-11 00:03:33 +0900

    [refactor] Database 설정 함수 이름 변경

--- src/main/kotlin/com/example/config/DatabaseConfig.kt
+++ src/main/kotlin/com/example/config/DatabaseConfig.kt
@@ -3,7 +3,7 @@ package com.example.config
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 
-fun Application.setupConfig() {
+fun Application.configureDatabase() {
     val databaseObject = environment.config.config("ktor.database")
     val driver = databaseObject.property("driver").getString()
     val url = databaseObject.property("url").getString()
