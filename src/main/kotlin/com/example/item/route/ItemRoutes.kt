ad6248d984b15a755bfe052978265148fa7a6cb7
ProtoSeo <proto_seo@naver.com>
2022-05-11 15:36:13 +0900

    [chore] thymeleaf 의존성 추가

--- src/main/kotlin/com/example/item/route/ItemRoutes.kt
+++ src/main/kotlin/com/example/item/route/ItemRoutes.kt
@@ -1,11 +1,16 @@
 package com.example.item.route
 
 import com.example.item.controller.ItemController
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.*
 
 fun Route.itemRoute(itemController: ItemController) {
     route("items") {
         get {
+            val items = itemController.getItems(call)
+            call.respond(ThymeleafContent("itemList", mapOf("items" to items)))
         }
         get("new") {
         }
