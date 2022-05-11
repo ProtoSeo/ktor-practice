eceb8d6b796fd552c0436b540a9adb46c2c620fc
ProtoSeo <proto_seo@naver.com>
2022-05-11 15:40:46 +0900

    [feat] Routing 관련 설정 추가 및 kodein 관련 설정 추가

--- src/main/kotlin/com/example/config/ModuleConfig.kt
+++ src/main/kotlin/com/example/config/ModuleConfig.kt
@@ -4,12 +4,15 @@ import com.example.item.controller.ItemController
 import com.example.item.domain.ItemRepository
 import com.example.item.service.ItemService
 import org.kodein.di.*
+import java.security.SecureRandom
+import kotlin.random.Random
 
 object ModuleConfig {
-    private val itemModule = DI.Module("ITEM") {
+    private val itemModule = DI.Module("Item") {
         bindSingleton { ItemController(instance()) }
-        bindSingleton { ItemService(instance()) }
+        bindSingleton { ItemService(instance(), instance()) }
         bindSingleton { ItemRepository() }
+        bindSingleton { SecureRandom() }
     }
 
     internal val kodein = DI {
--- src/main/kotlin/com/example/config/RouterConfig.kt
+++ src/main/kotlin/com/example/config/RouterConfig.kt
@@ -1,16 +1,27 @@
 package com.example.config
 
+import com.example.item.controller.ItemController
+import com.example.item.route.itemRoute
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.thymeleaf.*
+import org.kodein.di.instance
 
 fun Application.configureRouting() {
-    routing {
+    val itemController: ItemController by ModuleConfig.kodein.instance()
 
+    routing {
+        indexRoute()
+        itemRoute(itemController)
     }
 }
 
-fun Route.itemRoute() {
-    route("items") {
-
+fun Routing.indexRoute() {
+    route("/") {
+        get {
+            call.respond(ThymeleafContent("index", mapOf("host" to call.request.host())))
+        }
     }
 }
