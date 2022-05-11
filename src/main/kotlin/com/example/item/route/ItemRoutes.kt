94f8173f24c104c50a4e1cc0b6439b2b9c0a74de
ProtoSeo <proto_seo@naver.com>
2022-05-11 15:41:42 +0900

    [feat] Item 관련 routing 설정

--- src/main/kotlin/com/example/item/route/ItemRoutes.kt
+++ src/main/kotlin/com/example/item/route/ItemRoutes.kt
@@ -6,21 +6,49 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
 
-fun Route.itemRoute(itemController: ItemController) {
+fun Routing.itemRoute(itemController: ItemController) {
     route("items") {
-        get {
-            val items = itemController.getItems(call)
-            call.respond(ThymeleafContent("itemList", mapOf("items" to items)))
-        }
-        get("new") {
-        }
-        post {
-        }
-        get("{id}") {
-        }
-        get("{id}/edit") {
-        }
-        post("{id}") {
-        }
+        createItem(itemController)
+        readItem(itemController)
+        updateItem(itemController)
+        deleteItem(itemController)
+    }
+}
+
+private fun Route.createItem(itemController: ItemController) {
+    get("new") {
+        call.respond(ThymeleafContent("items/newItem", mapOf()))
+    }
+    post("new") {
+        call.respondRedirect(itemController.saveItem(call))
+    }
+    post("new/random") {
+        call.respondRedirect(itemController.saveRandomItems())
+    }
+}
+
+private fun Route.readItem(itemController: ItemController) {
+    get {
+        val items = itemController.getItems(call)
+        call.respond(ThymeleafContent("items/itemList", mapOf("items" to items)))
+    }
+}
+
+private fun Route.updateItem(itemController: ItemController) {
+    get("{id}/edit") {
+        val bookEditForm = itemController.getUpdateItemForm(call)
+        call.respond(ThymeleafContent("items/editItem", mapOf("form" to bookEditForm)))
+    }
+    post("{id}/edit") {
+        call.respondRedirect(itemController.updateItem(call))
+    }
+}
+
+private fun Route.deleteItem(itemController: ItemController) {
+    post("{id}/delete") {
+        call.respondRedirect(itemController.deleteItem(call))
+    }
+    post("delete") {
+        call.respondRedirect(itemController.deleteAllItem())
     }
 }
